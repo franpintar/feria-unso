e9cf8d8634966e49f83ef4e4d8ea8b9b5ff2b290
Juan Francisco Pintar <juanfranciscopintar@protonmail.com>
2025-08-03 18:12:11 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,13 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
 
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-  console.log('Server started on PORT:', PORT + '.');
-});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server started on PORT:', PORT + '.');
+  });
+})
