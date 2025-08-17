e2b9cc9c10d8ed853721b082149547ee30cdb78a
Juan Francisco Pintar <juanfranciscopintar@protonmail.com>
2025-08-16 23:33:24 -0300

    Fix issue with middleware order

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
-app.use(rateLimiter);
 app.use(
   cors({
     origin: 'http://localhost:5173',
   }),
 );
+app.use(express.json());
+app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
 
