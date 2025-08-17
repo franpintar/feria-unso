68d9afa07b17b049fad53f947822b7dc1295abcc
Juan Francisco Pintar <juanfranciscopintar@protonmail.com>
2025-08-16 23:13:21 -0300

    Add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from './routes/notesRoutes.ts';
 import { connectDB } from './config/db.ts';
-import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.ts';
 
 dotenv.config();
@@ -9,9 +11,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
 
 app.use('/api/notes', notesRoutes);
 
@@ -19,4 +25,4 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log('Server started on PORT:', PORT + '.');
   });
-})
+});
